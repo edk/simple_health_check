997265f9847ff973b8d9c1b5af241893f88348fd
Eddy Kim <eddyhkim@gmail.com>
2018-11-17 09:42:24 -0800

    New checks

--- lib/simple_health_check/redis_check.rb
+++ lib/simple_health_check/redis_check.rb
@@ -1,7 +1,21 @@
 class SimpleHealthCheck::RedisCheck < SimpleHealthCheck::Base
   def call(response:)
-    response.add name: 'svc_redis', status: 1
-    response.status_code = :ok
+    redis_check_proc = SimpleHealthCheck::Configuration.redis_check_proc
+    svc_name = "svc_redis"
+    if redis_check_proc && redis_check_proc.respond_to?(:call)
+      begin
+        # redis_check_proc is a required user-supplied function to see if connection is working.
+        rv = redis_check_proc.call
+        response.add name: svc_name, status: (rv || 'ok')
+        response.status_code = :ok
+      rescue
+        # catch exceptions since we don't want the health-check to bubble all the way to the otp
+        response.add name: 'redis_connection_error', status: $ERROR_INFO.message
+      end
+    else
+      response.add name: svc_name, status: 'missing_configuration'
+    end
+
     response
   end
 end
--- lib/simple_health_check/s3_check.rb
+++ lib/simple_health_check/s3_check.rb
@@ -1,7 +1,21 @@
 class SimpleHealthCheck::S3Check < SimpleHealthCheck::Base
   def call(response:)
-    response.add name: 'svc_s3', status: 1
-    response.status_code = :ok
+    s3_check = SimpleHealthCheck::Configuration.s3_check_proc
+    svc_name = "svc_s3"
+    if s3_check && s3_check.respond_to?(:call)
+      begin
+        # s3_check is a required user-supplied function to see if the connection is working.
+        rv = s3_check.call
+        response.add name: svc_name, status: (rv || 'ok')
+        response.status_code = :ok
+      rescue
+        # catch exceptions since we don't want the health-check to bubble all the way to the otp
+        response.add name: 'svc_connection_error', status: $ERROR_INFO.message
+      end
+    else
+      response.add name: svc_name, status: 'missing_configuration'
+    end
+
     response
   end
 end
